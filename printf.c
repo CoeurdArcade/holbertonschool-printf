22cb8c874a37ce06d96722c8429ecb981baa8bdd
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-25 16:02:47 +0200

    printf function

---
+++ printf.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...) {
+	va_list args;
+	int result;
+
+/**
+ *  Initialize the variable arguments list
+ */
+	va_start(args, format);
+
+/** Print the formatted string using vfprintf
+ *
+ */
+	result = vfprintf(stdout, format, args);
+	
+/**
+ *  Clean up the variable arguments list
+ */
+	va_end(args);
+
+	return result;
+}
+
+int main() {
+	_printf("Hello, world! The current year is %d.\n", 2023);
+	return 0;
+}
