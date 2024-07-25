20deef0cdb2bc504a1ccc5f5bdc8f88373020095
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-25 12:33:11 +0200

    function c

---
+++ print_c.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * printf_char - Prints a char.
+ * @val: The argument to print.
+ *
+ * Return: 1.
+ */
+int printf_char(va_list val)
+{
+    char c;
+
+    c = va_arg(val, int);
+    _putchar(c);
+    return (1);
+}
