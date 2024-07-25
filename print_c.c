8f29f09b3ab4d298d1e6dede375369e77ef10485
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-25 13:02:30 +0200

    betty check

--- print_c.c
+++ print_c.c
@@ -6,11 +6,12 @@
  *
  * Return: 1.
  */
+
 int printf_char(va_list val)
 {
-    char c;
+	char c;
 
-    c = va_arg(val, int);
-    _putchar(c);
-    return (1);
+	c = va_arg(val, int);
+	_putchar(c);
+	return (1);
 }
