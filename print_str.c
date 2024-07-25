52a9252b5bc7f0ddcac2e7228e4dfdc089fed74d
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-25 12:34:50 +0200

    str function

---
+++ print_str.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string pointer
+ *
+ * Return: The length of the string, or negative one (-1) if the input is NULL.
+ */
+int _strlen(char *str)
+{
+    if (str == NULL) {
+        return -1;
+    }
+
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+        ;
+
+    return (i);
+}
+
+/**
+ * _strlenc - _strlen function but applied for constant char pointer str.
+ * @str: char pointer
+ *
+ * Return: The length of the string, or negative one (-1) if the input is NULL.
+ */
+int _strlenc(const char *str)
+{
+    if (str == NULL) {
+        return -1;
+    }
+
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+        ;
+
+    return (i);
+}
