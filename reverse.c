9a312a617dfdf43f74549e3f6790c741727dcdee
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-26 17:28:40 +0200

    updated script

--- reverse.c
+++ reverse.c
@@ -3,27 +3,30 @@
 /**
  * reverse - a function that reverses a string
  * @str: string to reverse
- * it has to be allocated on memory before
  *
- * Return: reverse result
+ * Return: reverse result, or NULL if input is invalid
  */
 
 char *reverse(char *str)
 {
+	if (str == NULL || *str == '\0')
+	{
+		return NULL;
+	}
+
 	size_t len = _strlen(str);
 	char *rev = malloc((len + 1) * sizeof(char));
 	if (!rev)
 	{
-	perror("malloc failed");
-	exit(EXIT_FAILURE);
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
 	}
 
 	for (size_t i = 0; i < len; i++)
 	{
-	rev[i] = str[len - i - 1];
+		rev[i] = str[len - i - 1];
 	}
 	rev[len] = '\0';
 
 	return rev;
-
 }
