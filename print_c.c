7a0aa3fb4c52eaad7e90edad19cdc4df201a5ac6
evrade10 <safouevrade@gmail.com>
2024-07-26 09:18:21 +0000

    print_c.c

--- print_c.c
+++ print_c.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include "main.h"
 
 /**
@@ -6,12 +7,12 @@
  *
  * Return: 1.
  */
+
 int printf_char(va_list val)
 {
-    char c;
+	char c;
 
-    c = va_arg(val, int);
-    _putchar(c);
-    return (1);
-}
- 
+	c = va_arg(val, int);
+	_putchar(c);
+	return (1);
+} 
