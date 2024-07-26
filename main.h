e76eaae3297c7d0765fb78528f84015f87293e87
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-26 19:44:44 +0200

    updated proto

--- main.h
+++ main.h
@@ -18,11 +18,11 @@ typedef struct specfunc
 	int (*func)(va_list, int);
 } SF;
 
+int _putchar(char c)
 int _putchar_buf(char *buf, size_t len);
 int _printf(const char *format, ...);
 int printf_char(va_list val);
 int printf_string(va_list val);
-int _putchar(int c);
 char *convert_binary(int n);
 int is_string(const char *str);
 int print_s(va_list args);
