71e70b51b69537b1a2a629ba3d01b7636174ad28
evrade10 <safouevrade@gmail.com>
2024-07-26 09:09:29 +0000

    print_str

--- print_str.c
+++ print_str.c
@@ -1,34 +1,52 @@
+#include <stddef.h>
+#include <stdarg.h>
 #include "main.h"
 
 /**
  * _strlen - Returns the length of a string.
  * @str: string pointer
  *
- * Return: The length of the string.
+ * Return: The length of the string, or negative one (-1) if the input is NULL.
  */
+
 int _strlen(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        ;
+	if (str == NULL)
+	{
+		return -1;
+	}
 
-    return (i);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}	
+
+	return i;
 }
 
 /**
  * _strlenc - _strlen function but applied for constant char pointer str.
  * @str: char pointer
  *
- * Return: The length of the string.
+ * Return: The length of the string, or negative one (-1) if the input is NULL.
  */
+
 int _strlenc(const char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i++)
-        ;
+	if (str == NULL)
+	{
+		return -1;
+	}
 
-    return (i);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	return i;
 }
 
