c1273f377f08d1f64088ddd24731e5d66de5f4a8
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-26 23:49:22 +0200

    updated script to fix errors

--- printf.c
+++ printf.c
@@ -1,10 +1,11 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _printf - a function that produces output according to a format
  * @format: initializes the variable arguments list
- * prints the formatted string
+ * prints the formated string
  * cleans up the variable arguments list
  *
  * Return: Output result
@@ -13,11 +14,45 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int result;
+	int result = 0;
+
+	if (format == NULL)
+		return -1;
 
 	va_start(args, format);
 
-	result = vfprintf(stdout, format, args);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			break;
+
+			if (*format == 'c')
+			{
+				result += _putchar(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				result += printf_string(args);
+			}
+			else if (*format == '%')
+			{
+				result += _putchar('%');
+			}
+			else
+			{
+				result += _putchar('%')
+				result += _putchar(*format);
+			}
+		}
+		else
+		{
+			result += _putchar(*format);
+		}
+		format++;
+	}
 
 	va_end(args);
 
