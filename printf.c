73d4694c9004e527e3a0e6588426446657fdfe7c
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-26 10:06:06 +0200

    betty style

--- printf.c
+++ printf.c
@@ -1,23 +1,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * _printf - a function that produces output according to a format
+ * @va_list: initializes the variable arguments list
+ * @vfprint: prints the formatted string
+ * @va_end: cleans up the variable arguments list
+ *
+ * Return: Output result
+ */
+
 int _printf(const char *format, ...) {
 	va_list args;
 	int result;
 
-	/**
-	 *  Initialize the variable arguments list
-	 */
 	va_start(args, format);
 
-	/** Print the formatted string using vfprintf
-	 *
-	 */
 	result = vfprintf(stdout, format, args);
 
-	/**
-	 *  Clean up the variable arguments list
-	 */
 	va_end(args);
 
 	return result;
