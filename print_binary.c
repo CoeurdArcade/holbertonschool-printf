3b4918c004bb053c92c14dcc76b5e76112d225e2
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-26 16:18:46 +0200

    updated script errors

--- print_binary.c
+++ print_binary.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * convert_binary - a function that takes an integer as its argument
- * to convert to a pointer to a new null-terminated string with the
- * integer as a binary number
+ * convert_binary - converts an integer to a binary string
  * @n: integer to convert
+ * first calculate the length of the binary string
+ * second handle negative numbers
+ * third convert to binary and store in the string
  *
  * Return: integer represented as a binary number
  */
@@ -12,33 +13,44 @@
 char *convert_binary(int n)
 {
 	int neg = 0;
+	size_t len = 1;
+	char *str;
+
 	if (n < 0)
 	{
-	neg = 1;
-	n = -n;
+		neg = 1;
+		n = -n;
 	}
 
-	size_t len = 1;
-	int j = n;
-	while (j > 1)
+	for (size_t j = n; j > 1; j /= 2)
 	{
-	len++;
-	j /= 2;
+		len++;
 	}
 
-	char *str = malloc((len + neg + 1) * sizeof(char));
+	str = malloc((len + neg + 1) * sizeof(char));
 	if (!str)
 	{
-	perror("malloc failed");
-	exit(EXIT_FAILURE);
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
 	}
 
 	str[neg] = '0';
-	for (size_t = 0; l < len/2 + neg; l++)
+	if (neg)
 	{
-	char tmp = str[l];
-	str[l] = str[len - l - 1 + neg];
-	str[len - l - 1 + neg] = tmp;
+	str[0] = "1";
+	}
+
+	for (size_t i = 0; i < len/2 + neg; i++)
+	{
+		char tmp = str[i];
+		str[i] = str[len - i - 1 + neg];
+		str[len - i - 1 + neg] = tmp;
+	}
+
+	for (size_t i = len/2 + neg; i < len + neg; i++)
+	{
+		str[i] = n % 2 ? '1' : '0';
+		n /= 2;
 	}
 
 	return str;
