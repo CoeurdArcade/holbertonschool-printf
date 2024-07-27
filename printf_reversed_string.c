775dd59701d88384c169fd723af331c315c51d5f
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 09:08:34 +0200

    uppppppp

--- printf_reversed_string.c
+++ printf_reversed_string.c
@@ -12,11 +12,29 @@
 int printf_reversed_string(const char *format, ...)
 {
 	va_list args;
-	va_start(args, format);
+	const char *str;
 
-	const char *str = va_arg(args, const char *);
-	printf(format, str);
+	va_start(args, const char *);
+
+	int length = 0;
+	const char *ptr = str;
+
+	while (*ptr++)
+	{
+		length++;
+	}
+
+	for (int i =length - 1; i >= 0; i--)
+	{
+		putchar(str[i]);
+	}
 
 	va_end(args);
 	return 0;
+}
+
+int main()
+{
+    printf_reversed_string("%s", "Hello, World!");
+    return 0;
 }
