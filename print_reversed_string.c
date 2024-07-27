bb26a90bd0fc04ea1da9ba7b9af39150e28f7450
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 03:50:14 +0200

    fixing errors

--- print_reversed_string.c
+++ print_reversed_string.c
@@ -11,40 +11,41 @@
  */
 int printf_reversed_string(va_list val)
 {
-    char *str;
-    size_t length;
-    int count = 0;
+	char *str;
+	size_t length;
+	int count = 0;
 
-    str = va_arg(val, char *);
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	str = va_arg(val, char *);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    length = _strlen(str);
-    char *reversed = malloc(length + 1);
+	length = _strlen(str);
+	char *reversed = malloc(length + 1);
 
-    if (!reversed)
-    {
-        free(reversed);
-        return -1;
-    }
+	if (!reversed)
+	{
+		free(reversed);
+		return -1;
+	}
 
-    for (size_t i = 0; i < length; i++)
-    {
-        reversed[i] = str[length - i - 1];
-    }
-    reversed[length] = '\0';
+	for (size_t i = 0; i < length; i++)
+	{
+		reversed[i] = str[length - i - 1];
+	}
+	reversed[length] = '\0';
 
-    for (size_t i = 0; i < length; i++)
-    {
-        if (_putchar(reversed[i]) == -1)
-        {
-            free(reversed);
-            return -1;
-        }
-    }
+	for (size_t i = 0; i < length; i++)
+	{
+		if (_putchar(reversed[i]) == -1)
+		{
+			free(reversed);
+			return -1;
+		}
+	}
 
-    free(reversed);
-    return length;
+	free(reversed);
+	return length;
 }
+
