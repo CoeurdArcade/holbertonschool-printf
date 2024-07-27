20a5cc6894d021a30ff90fd7b7d32547b51ffae8
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 02:38:10 +0200

    updated script to fix errors

--- print_c.c
+++ print_c.c
@@ -7,25 +7,14 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
 int printf_char(va_list val)
 {
-	int c;
+    int c;
 
-	/* Retrieve the next argument from the variable argument list */
-	c = va_arg(val, int);
+    c = va_arg(val, int);
 
-	/* Check if the value is a valid character */
-	if (c < 0 || c > 127)
-	{
-	/* If it's not, print an error message and return an error code */
-	_putchar('%');
-	_putchar('?');
-	return -1;
-	}
+    if (_putchar(c) == -1)
+        return -1;
 
-	/* Print the character to the standard output */
-	_putchar(c);
-	return 1;
-
+    return 1;
 }
