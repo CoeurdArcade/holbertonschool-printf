55a60494a597a2702d10e72785bdbce2d56463be
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 02:36:51 +0200

    updated script to fix errors

--- putchar.c
+++ putchar.c
@@ -5,13 +5,42 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1
- * On error, -1 is returned, end errno is set appropriately
+ * Return: On success 1, on error, -1 is returned, and errno is set appropriately
  */
-
 int _putchar(char c)
 {
-	if (write(1, &c, 1) == -1)
-		return -1;
-	return 1;
+    static char buffer[1024];
+    static int buffer_index = 0;
+
+    buffer[buffer_index++] = c;
+
+    if (buffer_index >= 1024)
+    {
+        if (write(1, buffer, buffer_index) == -1)
+            return -1;
+        buffer_index = 0;
+    }
+
+    return 1;
 }
+
+/**
+ * _flush_buffer - flushes the buffer to stdout
+ *
+ * Return: On success, the number of bytes written. On error, -1 is returned.
+ */
+int _flush_buffer()
+{
+    static char buffer[1024];
+    static int buffer_index = 0;
+
+    if (buffer_index > 0)
+    {
+        if (write(1, buffer, buffer_index) == -1)
+            return -1;
+        buffer_index = 0;
+    }
+
+    return 1;
+}
+
