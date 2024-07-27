2285b92d435831039fcb13ca69769e25e8130e70
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 03:52:06 +0200

    up to fixxxx

--- print_pointer.c
+++ print_pointer.c
@@ -10,35 +10,36 @@
  */
 int printf_pointer(va_list val)
 {
-    void *ptr = va_arg(val, void *);
-    unsigned long int addr = (unsigned long int)ptr;
-    int count = 0;
+	void *ptr = va_arg(val, void *);
+	unsigned long int addr = (unsigned long int)ptr;
+	int count = 0;
 
-    if (_putchar('0') == -1)
-        return -1;
-    if (_putchar('x') == -1)
-        return -1;
-    count += 2;
+	if (_putchar('0') == -1)
+		return -1;
+	if (_putchar('x') == -1)
+		return -1;
+	count += 2;
 
-    char buffer[16]; // 16 characters for a 64-bit address in hexadecimal
-    int i = 15;
+	char buffer[16]; /* 16 characters for a 64-bit address in hexadecimal */
+	int i = 15;
 
-    while (addr > 0)
-    {
-        buffer[i--] = (addr % 16) + ((addr % 16 > 9) ? 'A' - 10 : '0');
-        addr /= 16;
-    }
+	while (addr > 0)
+	{
+		buffer[i--] = (addr % 16) + ((addr % 16 > 9) ? 'A' - 10 : '0');
+		addr /= 16;
+	}
 
-    for (; i >= 0; i--)
-    {
-        buffer[i] = '0';
-    }
+	for (; i >= 0; i--)
+	{
+		buffer[i] = '0';
+	}
 
-    for (i = 0; i < 16; i++)
-    {
-        if (_putchar(buffer[i]) == -1)
-            return -1;
-    }
+	for (i = 0; i < 16; i++)
+	{
+		if (_putchar(buffer[i]) == -1)
+			return -1;
+	}
 
-    return count + 16;
+	return count + 16;
 }
+
