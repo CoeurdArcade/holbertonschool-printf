efa89dd468632c736b87c4ba7796e94ef3653a60
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 03:41:19 +0200

    betty betty

--- print_s.c
+++ print_s.c
@@ -10,14 +10,14 @@
  */
 size_t _strlen(const char *s)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    return len;
+	return len;
 }
 
 /**
@@ -28,20 +28,20 @@ size_t _strlen(const char *s)
  */
 int printf_string(va_list val)
 {
-    char *str;
-    size_t length;
+	char *str;
+	size_t length;
 
-    str = va_arg(val, char *);
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	str = va_arg(val, char *);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    length = _strlen(str);
-    for (size_t i = 0; i < length; i++)
-    {
-        if (_putchar(str[i]) == -1)
-            return -1;
-    }
-    return length;
+	length = _strlen(str);
+	for (size_t i = 0; i < length; i++)
+	{
+		if (_putchar(str[i]) == -1)
+			return -1;
+	}
+	return length;
 }
