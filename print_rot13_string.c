bd3e281a69c08c7779b3f6e277181819e063ea86
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 03:48:47 +0200

    betttyyyyyyyyy

--- print_rot13_string.c
+++ print_rot13_string.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 #include "main.h"
 
@@ -10,40 +11,41 @@
  */
 int printf_rot13_string(va_list val)
 {
-    char *str;
-    size_t length;
-    int count = 0;
+	char *str;
+	size_t length;
+	int count = 0;
 
-    str = va_arg(val, char *);
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	str = va_arg(val, char *);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
 
-    length = _strlen(str);
-    char *rot13 = malloc(length + 1);
+	length = _strlen(str);
+	char *rot13 = malloc(length + 1);
 
-    if (!rot13)
-    {
-        free(rot13);
-        return -1;
-    }
+	if (!rot13)
+	{
+		free(rot13);
+		return -1;
+	}
 
-    for (size_t i = 0; i < length; i++)
-    {
-        rot13[i] = str[(length - i - 1 + 13) % 26];
-    }
-    rot13[length] = '\0';
+	for (size_t i = 0; i < length; i++)
+	{
+		rot13[i] = str[(length - i - 1 + 13) % 26];
+	}
+	rot13[length] = '\0';
 
-    for (size_t i = 0; i < length; i++)
-    {
-        if (_putchar(rot13[i]) == -1)
-        {
-            free(rot13);
-            return -1;
-        }
-    }
+	for (size_t i = 0; i < length; i++)
+	{
+		if (_putchar(rot13[i]) == -1)
+		{
+			free(rot13);
+			return -1;
+		}
+	}
 
-    free(rot13);
-    return length;
+	free(rot13);
+	return length;
 }
+
