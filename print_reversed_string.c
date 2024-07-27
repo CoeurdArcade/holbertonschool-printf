f52afdaedaad3cf4d241f831fdeb963c1e4cf3df
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 06:35:21 +0200

    remove error

--- print_reversed_string.c
+++ print_reversed_string.c
@@ -46,18 +46,3 @@ int printf_string(va_list val)
 	}
 	return length;
 }
-
-void printf_reversed_string(const char *str)
-{
-	if (str == NULL)
-		return;
-
-	const char *end = str;
-	while (*end) ++end;
-
-	for (--end; end >= str; --end)
-	{
-		putchar(*end);
-	}
-	putchar('\n');
-}
