99ae5b7234d720f21ac9fb9d83de924ca02e6505
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 02:39:10 +0200

    updated script to fix errors

--- print_s.c
+++ print_s.c
@@ -1,26 +1,6 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include <ctype.h>
+#include <string.h>
 #include "main.h"
-
-/**
- * is_string - checks if a given argument is a string
- * @str: the argument to check
- * 
- * Return: 1 if the argument is a string, 0 otherwise.
- */
-int is_string(const char *str)
-{
-	for (; *str != '\0'; str++)
-	{
-	if (!isprint(*str))
-		{
-			return 0;
-		}
-	}
-
-	return 1;
-}
 
 /**
  * _strlen - calculates the length of a string
@@ -30,32 +10,14 @@ int is_string(const char *str)
  */
 size_t _strlen(const char *s)
 {
-	size_t len = 0;
+    size_t len = 0;
 
-	while (s[len] != '\0')
-	{
-		len++;
-	}
+    while (s[len] != '\0')
+    {
+        len++;
+    }
 
-	return len;
-}
-/**
- * print_s - prints a string
- * @args: the va_list of arguments to print
- *
- * Return: The number of characters printed
- */
-
-int print_s(va_list args)
-{
-	char *str = va_arg(args, char *);
-	size_t len;
-
-	if (str == NULL)
-		str = "(null)";
-
-	len = _strlen(str);
-	return _putchar_buf(str, len);
+    return len;
 }
 
 /**
@@ -66,25 +28,20 @@ int print_s(va_list args)
  */
 int printf_string(va_list val)
 {
-	char *str;
-	int i, length;
+    char *str;
+    size_t length;
 
-	str = va_arg(val, char *);
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
+    str = va_arg(val, char *);
+    if (str == NULL)
+    {
+        str = "(null)";
+    }
 
-	if (!is_string(str))
-	{
-		return -1;
-	}
-
-	length = _strlen(str);
-	for (i = 0; i < length; i++)
-	{
-	if (_putchar(str[i]) == -1)
-		return -1;
-	}
-	return length;
+    length = _strlen(str);
+    for (size_t i = 0; i < length; i++)
+    {
+        if (_putchar(str[i]) == -1)
+            return -1;
+    }
+    return length;
 }
