f0de34650f954eed752b0c2e9fbc0e56a3da2369
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-27 05:55:38 +0200

    proto added

--- main.h
+++ main.h
@@ -20,5 +20,6 @@ int printf_reversed_string(va_list val);
 int printf_rot13_string(va_list val);
 size_t _strlen(const char *s);
 int _flush_buffer(void);
+int _flush_buffer(int count);
 
 #endif /* MAIN_H */
