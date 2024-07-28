9c3801daf54290dd1a632ea877b998e99c898a88
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-28 10:15:22 +0200

    fix fff

--- print_c.c
+++ print_c.c
@@ -8,11 +8,11 @@
  * Return: 1.
  */
 
-int _char(va_list val, int)
+int _char(va_list val, int flag)
 {
 	char c;
 
-	c = va_arg(val, int);
+	c = va_arg(val, int flag);
 	_putchar(c);
 	return (1);
 }
