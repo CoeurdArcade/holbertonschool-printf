2127e3600ac6155d427016f5969c96779bcfc754
CoeurdArcade <coeurdarcade@gmail.com>
2024-07-28 10:08:18 +0200

    update

--- main.h
+++ main.h
@@ -18,21 +18,14 @@ typedef struct specfunc
 
 /* Function prototypes */
 
-int is_string(const char *str);
-int printf_string(va_list val);
-size_t size_t_strlen(const char *s);
 int _putchar(int c);
-void printf_reversed_string(const char *format, const char *str);
 int _printf(const char *format, ...);
-int _string(va_list val, int flag);
-int _int(va_list val, int flag);
-int _char(va_list val, int flag);
-int _binary(va_list val, int flag);
-char *itoa(int i);
-char *reverse(char *str);
-char *convert_binary(int num);
 int _strlen(char *str);
 int _strlenc(const char *str);
+int is_string(const char *str);
+size_t size_t_strlen(const char *s);
+int _putchar(int c);
+int printf_string(va_list val);
 int print_37(void);
 
 #endif /* MAIN_H */
